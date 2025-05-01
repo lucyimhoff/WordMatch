58b7359bac567567045a64e5290ebd2392365da3
lucyimhoff <lucyi4@nycstudents.net>
2025-05-01 18:26:55 +0000

    scoreGuess complete

--- WordMatch.java
+++ WordMatch.java
@@ -1,4 +1,21 @@
 public class WordMatch{
     private String secret;
+
+    public WordMatch(String word){
+        secret = word;
+
+    }
+
+    public int scoreGuess(String guess){
+        int score = 0;
+        int count = 0;
+        int guesslen = guess.length();
+        for(int i = 0; i <= secret.length() - guesslen; i++){
+            if((secret.substring(i, i + guesslen).equals(guess))) count++;
+        }
+        score = count * (guesslen) * (guesslen);
+        return score;
+
+    }
     
 }
